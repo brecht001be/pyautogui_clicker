e0c13b469c8f9a916f4f2a2858fe2c10e7bf291f
Brecht Dervaux <brecht.dervaux@gmail.com>
2021-12-12 16:01:14 +0100

    added another comment line

--- pygui.py
+++ pygui.py
@@ -15,7 +15,7 @@ def randomSleep():
 # moves to (1717,352) in 1 sec
 def goToAndClick(coords, duration):
     pyautogui.moveTo(coords[0] + random.randint(0,4), coords[1] + random.randint(0,4), duration)
-    pyautogui.click()
+    pyautogui.click() # another comment added here
  
 while True:
   print("Clicked Advertisement...")
